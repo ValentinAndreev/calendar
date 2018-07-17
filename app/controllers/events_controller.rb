d7111e099bd15194f8641022b11a530ca6292238
ValentinAndreev <valentinandreev80@gmail.com>
2018-07-17 11:37:51 +0300

    User can see all events or only own events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,10 +2,16 @@
 
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    events = Event.all
+    render :index, locals: { events: events, message: 'All events' }
   end
 
   def show
     @event = Event.find(params[:id])
+  end
+
+  def my
+    my_events = Event.where(user: current_user)
+    render :index, locals: { events: my_events, message: 'My events' }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-  resources :events
+  resources :events do
+    get 'my', on: :collection
+  end
 end
