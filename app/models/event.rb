89abde0806fd7da3d5a652b3aa551f9f277cb462
ValentinAndreev <valentinandreev80@gmail.com>
2018-07-17 10:15:27 +0300

    Fix relationship and validation for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class Event < ApplicationRecord
   belongs_to :user
+  validates :name, :start_time, presence: true
 end
--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :event do
-    name "MyString"
-    date "2018-07-17 09:57:40"
+    name 'MyString'
+    date '2018-07-17 09:57:40'
     user nil
   end
 end
