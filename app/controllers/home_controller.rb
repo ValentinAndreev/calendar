1c247e1b39b46e742a0b02d9afccfd19c41b34a1
ValentinAndreev <valentinandreev80@gmail.com>
2018-07-17 10:10:32 +0300

    Introduce event model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @events = Event.all
+  end
 end
---
+++ spec/factories/events.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event do
+    name "MyString"
+    date "2018-07-17 09:57:40"
+    user nil
+  end
+end
